917a7e57ddeb98278e42c6d886121ff168ac2f25
Jaime Londoño <jaime951@gmail.com>
2017-05-09 15:02:45 +0200

    Initial commit basic Spring Boot application

---
+++ src/main/java/it/polito/ai/Laboratorio4Application.java
@@ -0,0 +1,12 @@
+package it.polito.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio4Application.class, args);
+	}
+}
